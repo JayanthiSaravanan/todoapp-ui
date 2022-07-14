8e6122713dd1012b0561212a3f2e297e4e309a75
Jayanthi <jayanthi.saravanann@gmail.com>
2022-07-14 17:37:07 +0530

    feature added in registration

--- js/register.js
+++ js/register.js
@@ -2,12 +2,12 @@
 function register() {
     event.preventDefault();
 
-    let fname = document.querySelector("#fname").value;
+    const fname = document.querySelector("#fname").value;
     // alert('result:' + name);
-    let lname = document.querySelector("#lname").value;
-    let email = document.querySelector("#email").value;
-    let gender = "";//document.querySelector("#gender").value;
-    var ele = document.getElementsByName('gender');
+    const lname = document.querySelector("#lname").value;
+    const email = document.querySelector("#email").value;
+    const gender = "";//document.querySelector("#gender").value;
+    const ele = document.getElementsByName('gender');
 
     for (i = 0; i < ele.length; i++) {
         if (ele[i].checked) {
@@ -15,34 +15,34 @@ function register() {
         }
     }
 
-    let address = document.querySelector("#address").value;
+    const address = document.querySelector("#address").value;
 
-    let city = document.querySelector("#city").value;
-    let state = document.querySelector("#state").value;
-    let form = document.querySelector("#regform");
+    const city = document.querySelector("#city").value;
+    const state = document.querySelector("#state").value;
+    const form = document.querySelector("#regform");
 
 
     // alert('result:' + state);
 
     if (fname == null || fname.trim() == "") {
-        alert("enter the valid firstname")
+        alert("Enter the valid firstname")
         return false;
     }
     if (fname.length < 3) {
-        alert("Name must be minimum 3 characters");
+        alert("First Name must be minimum 3 characters");
         return false;
     }
     if (lname == null || lname.trim() == "") {
-        alert("enter last name")
+        alert("Enter last name")
         return false;
     }
     if (email == null || email.trim() == "") {
-        alert("enter the valid emailid")
+        alert("Enter the valid emailid")
         return false;
     }
 
 
-    const todoobj = {
+    const userObj = {
         fname: fname,
         lname: lname,
         email: email,
@@ -51,17 +51,21 @@ function register() {
         city: city,
         state: state,
         created_by: "Jayanthi",
-        created_date: new Date().toJSON()
+        created_date: new Date().toJSON(),
+        modified_by: "Jayanthi",
+        modified_date: new Date().toJSON(),
+        role: "user"
 
 
 
     };
-    console.log(todoobj);
+    console.log(userObj);
     const url = "http://localhost:3000/users"
 
-    axios.post(url, todoobj).then(res => {
-        alert("successfully logged in");
-        form.reset();
+    axios.post(url, userObj).then(res => {
         console.log(res.data);
+        alert("successfully Registered");
+        form.reset();
+
     });
 }
